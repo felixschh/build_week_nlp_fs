227472163bae89cce430f710dce9f46929366c73
Femi Oyeniyi <jolaoduwole@gmail.com>
2022-07-28 20:30:51 +0300

    created table to append prediction

--- eunice/app.py
+++ eunice/app.py
@@ -9,24 +9,25 @@ db = SQLAlchemy()
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comments = db.Column(db.String(150), nullable=False)
-    toxis = db.Column(db.String(150), nullable=False)
-    severe_toxic = db.Column(db.Integer, nullable=False)
-    obscene = db.Column(db.Integer, nullable=False)
-    threat = db.Column(db.Integer, nullable=False)
-    insult= db.Column(db.String(150), nullable=False)
+    prediction = db.Column(db.String(150), nullable=False)
+    # severe_toxic = db.Column(db.Integer, nullable=False)
+    # obscene = db.Column(db.Integer, nullable=False)
+    # threat = db.Column(db.Integer, nullable=False)
+    # insult= db.Column(db.String(150), nullable=False)
 
 def dataa():
     comment = request.form.get('comment_text')
-    # sex = request.form.get('toxic')
-    # age = request.form.get('severe_toxic')
-    # sib = request.form.get('obscene')
-    # parch = request.form.get('threat')
-    # embark = request.form.get('insult')
-    # identity = request.form.get('identity_hate')
-    Dataa = Data(comments = comment)
+    # toxis  = 'toxic'
+    # severe_toxic= 'severe_toxic'
+    # obscene = 'obscene'
+    # threat = 'threat'
+    # insult = 'insult'
+    pred = 'toxic'
+    # Dataa = Data(comments = comment,toxis  = toxis, severe_toxic= severe_toxic, obscene = obscene, threat= threat, insult=insult)
+    Dataa = Data(comments = comment, prediction=pred)
     db.session.add(Dataa)
     db.session.commit()
-    # return 'sucessful'
+#     return pred
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///modeldata.db'
